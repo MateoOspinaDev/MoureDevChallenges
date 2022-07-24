2b924469dea06073628451ffb809166ad2471a1e
Mateo Ospina <mateoospinadesarrollo@gmail.com>
2022-07-24 12:35:34 -0500

    Challenge1 refactor

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
+Y
+)src/Challenge4PoligonArea/Challenge4.java,9\4\94ab86a9d3be7ecffbc7fb4a54bd3ab3fab59462
+V
+&src/Challenge4PoligonArea/Poligon.java,8\f\8f1ee791f0e2619d4ca32e61ca7c2b05da903fde
+W
+'src/Challenge4PoligonArea/Triangle.java,5\e\5e4227cc204ae3c8e70de745f7a14951253a6134
+X
+(src/Challenge4PoligonArea/Rectangle.java,8\7\87a15ce4154328ae9765f617d094a17fc9e0b262
+U
+%src/Challenge4PoligonArea/Square.java,2\8\287a1abc300e87e4e9b33e769c99e144df4f252d
--- src/Challenge1Anagrama.java
+++ src/Challenge1Anagrama.java
@@ -23,6 +23,39 @@ public class Challenge1Anagrama {
      * - Subiré una posible solución al ejercicio el lunes siguiente al de su publicación.
      *
      */
+    //Una mejor  forma
+    static boolean isAnagramOptim(String a, String b){
+        if(a.length()!=b.length()) return  false;
+        a=a.toLowerCase();
+        b=b.toLowerCase();
+        Map<Character,Integer> map = new HashMap<>();
+
+        for (int i = 0; i <b.length() ; i++) {
+            char letra = b.charAt(i);
+            if (!map.containsKey(i)){
+                map.put(letra,1);
+            }else{
+                Integer frecuencia = map.get(letra);
+                map.put(letra,++frecuencia);
+            }
+        }
+        for (int i = 0; i < a.length(); i++) {
+            char letra = a.charAt(i);
+
+            if (!map.containsKey(letra)) return false;
+
+            Integer frecuencia  = map.get(letra);
+
+            if(frecuencia==0) return false;
+            else {
+                map.put(letra,--frecuencia);
+            }
+        }
+
+        return true;
+    }
+
+
     static Boolean esAnagrama(String palabra1, String palabra2) {
         if((palabra1.length()!=palabra2.length() || palabra1.equals(palabra2))) return false;
 
@@ -42,5 +75,13 @@ public class Challenge1Anagrama {
         System.out.println(esAnagrama("hola","aloh"));
         System.out.println(esAnagrama("hola","ahol"));
         System.out.println(esAnagrama("hola","ahola"));
+
+        System.out.println(isAnagramOptim("hola","hola"));
+        System.out.println(isAnagramOptim("hola","aloh"));
+        System.out.println(isAnagramOptim("hola","ahol"));
+        System.out.println(isAnagramOptim("hola","ahola"));
     }
+
+
+
 }
